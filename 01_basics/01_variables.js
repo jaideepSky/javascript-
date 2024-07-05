83a9cbb8116fa6e0ecc7b5503adcf6250f2210cf
jaideepSky <168081364+jaideepSky@users.noreply.github.com>
2024-07-05 08:11:27 +0000

    check

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail = "jaideep@google.com"
+var accountPassword = "12345"
+accountCity = "Yamuna Nagar"
+
+// accountId = 2
+// console.log(accountId);
+accountEmail = "js@google.com"
+accountPassword = "55434"
+accountCity = "Patiala"
+let accountstate ;
+
+console.log(accountEmail);
+
+console.table([accountEmail , accountId ,accountPassword , accountCity ,accountstate ]) 
+
+// Prefer not to use var 
+// because of issue in block scope and functional scope
