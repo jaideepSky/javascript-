ab75e0850d557f457833b3511245477faef03601
jaideepSky <168081364+jaideepSky@users.noreply.github.com>
2024-07-05 11:29:31 +0000

    datatypes

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,25 @@
+"use strict"; // treat  all JS as a newer version 
+
+// alert(3+3) // we are using node js not browser
+
+console.log(3+3)
+console.log("checking")
+
+let name = "jaideep"
+let age = 54
+let isloggedin = false
+let isloggedout = true
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true / false 
+// null => standalone value 
+// undefined => not assign value 
+// symbol => unique
+// object 
+
+console.log(typeof "jaideep");
+console.log(typeof age);
+console.log(typeof null); //object 
+console.log(typeof undefined); //undefined
