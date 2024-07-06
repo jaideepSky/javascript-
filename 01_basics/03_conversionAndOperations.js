2c2cf3ead2547fc9c126b2ed16b9b82e1c6d9daf
jaideepSky <168081364+jaideepSky@users.noreply.github.com>
2024-07-06 07:00:29 +0000

    conversiion of a datatype

---
+++ 01_basics/03_conversionAndOperations.js
@@ -0,0 +1,27 @@
+let score = "55acj"
+console.log(typeof score);
+
+let valueInNumber  = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "55" => 55
+// "55acj" => Nan -> not a number
+// true => 1
+// false => 0
+
+let isLogedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLogedIn)
+console.log(typeof booleanIsLoggedIn);
+console.log( booleanIsLoggedIn);
+
+// 1 => true 
+// 0 => false
+// "" => false
+// "jaideep " => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log( typeof stringNumber);
