65408cd7b8f80b252b3a99cf38690ce77d17cbc9
jaideepSky <168081364+jaideepSky@users.noreply.github.com>
2024-07-07 12:59:23 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,20 @@
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null > 0 );
+console.log(null == 0);
+console.log(null >= 0);
+
+
+ //  comparision convert null to a number treating it as 0 //
+       // thats  why null>=0 is true and null > 0 is false //
+        
+
+       console.log(undefined == 0);
+       console.log(undefined > 0);
+       console.log(undefined < 0);
+
+       // always give false //
+
+       console.log("2" === 2);
+       
