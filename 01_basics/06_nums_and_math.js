b4ea621b876e400594e7d42d2f65c51acfda7b9f
jaideepSky <168081364+jaideepSky@users.noreply.github.com>
2024-07-09 11:49:27 +0000

    nums and maths

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,25 @@
+const score = 400
+// console.log(score);
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 23.8966
+// console.log(otherNumber.toPrecision(4));
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-In'));
+
+
+// console.log(Math);
+// console.log(Math.abs(-5));
+
+
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min =10
+const max = 20
+
+console.log((Math.random() *  (max - min + 1) ) + min ) 
