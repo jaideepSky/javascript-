8a0735dddf89d993e79860f7a571c5653d585218
jaideepSky <168081364+jaideepSky@users.noreply.github.com>
2024-07-10 11:44:52 +0000

    date and time

---
+++ 01_basics/07_dateAndTime.js
@@ -0,0 +1,23 @@
+// Dates 
+
+let myDates = new Date()
+ 
+// console.log(myDates.toString());
+// console.log(myDates.toDateString());
+// console.log(myDates.toJSON());
+// console.log(myDates.toISOString());
+// console.log(myDates.toLocaleString());
+
+console.log(typeof myDates); // object
+
+// let myCreatedDate  = new Date(2024 , 0  ,23 )
+// console.log(myCreatedDate.toDateString());/
+// let myCreatedDate  = new Date(2024 , 0  ,23 , 5 ,3)
+// console.log(myCreatedDate.toLocaleString());
+// let myCreatedDate  = new Date("01-14-2024" )
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
+
+
