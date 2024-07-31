defc87021910021dee923169382155b6b8579b9d
jaideepSky <168081364+jaideepSky@users.noreply.github.com>
2024-07-31 15:00:52 +0000

    if and else

---
+++ 04_controlFlow/one.js
@@ -0,0 +1,55 @@
+// if
+// conditions --> < , > , <= , >= , == , != , === , !==
+
+// const isUserLoggedIn = true
+// const temperature = 30
+// if(temperature < 50){
+//     console.log(`${temperature} is less than 50`);
+
+// }
+// else{
+// console.log(`${temperature} is greater than 50`);
+// }
+
+// const score = 200
+
+// if(score > 100){
+//     const power = "fly"
+//     console.log(`User power : ${power}`);
+
+// }
+
+
+// const balance = 1000
+
+// if(balance < 500){
+//     console.log("less than ");
+
+// }
+// else if(balance<750){
+//     console.log("less than 750");
+
+// }
+// else if(balance<900){
+//     console.log("less than 900");
+
+// }
+// else {
+//     console.log("less than 1700");
+// }
+
+// const userLoggedIn = true
+// const debitCard = true
+
+// if(userLoggedIn && debitCard){
+//     console.log("Allow to buy courses");
+
+// }
+
+// const loggdInFromGoogle = false
+// const loggedInFromEmail = true
+
+// if(loggdInFromGoogle || loggedInFromEmail){
+//     console.log("you can loggin");
+
+// }
