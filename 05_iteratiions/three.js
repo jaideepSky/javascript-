129f4994f58995357e9909ff7559f4199d4b259e
jaideepSky <168081364+jaideepSky@users.noreply.github.com>
2024-08-03 08:43:10 +0000

    for each loop

---
+++ 05_iteratiions/three.js
@@ -0,0 +1,40 @@
+const coding = ["js","ruby", "java" , "cpp", "python"]
+
+// coding.forEach( function (val) {
+//     console.log(val);
+// } 
+
+// )
+
+// coding.forEach( (items) => {
+//     console.log(items);
+    
+// })
+
+// coding.forEach((items , index  , arr) => {
+//     console.log(items , index , arr);
+    
+
+// })
+
+const myCoding = [
+{
+    languageName : "javascript",
+    languageFileName : "js"
+},
+{
+    languageName : "java",
+    languageFileName : "java"
+},
+{
+    languageName : "python",
+    languageFileName : "py"
+}
+]
+
+myCoding.forEach( (item) => {
+console.log(item.languageName);
+
+})
+    
+
