0d9818d183c44d4a6eb606c2106a87683b76ddc0
jaideepSky <168081364+jaideepSky@users.noreply.github.com>
2024-08-04 07:43:00 +0000

    for each loop

---
+++ 05_iteratiions/four.js
@@ -0,0 +1,36 @@
+// const coding = ["js" , "ruby" , "java","python","cpp"]
+
+// const values = coding.forEach( (items)=>{
+//     // console.log(items);
+//     return items
+    
+// })
+
+// console.log(values);
+
+// const myNums = [1 ,2 ,3 ,4 ,5 ,6 ,7 ,8 ,9 ,10] 
+
+
+//  const newNums = myNums.filter((num)=> {
+    //      return num > 4
+    //  }
+    // )
+    // console.log(newNums);
+    
+    // 
+    
+    const myNumbers = [1 ,2 ,3 ,4 ,5 ,6 ,7 ,8 ,9 ,10] 
+
+//  const add = myNumbers.map( (num) => {
+//     return num+10 
+// })
+
+// console.log(add);
+
+
+// const newNums = myNumbers.map( (num) => num*10)
+// .map((num) => num + 1 )
+
+
+const myNums = [1,2,3]
+myn
